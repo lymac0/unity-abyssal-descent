076f1e265499c17a6ab27eb80ec5a4d9686bc4ed
Kamil Sert <srtkamil42@gmail.com>
2025-02-09 18:28:08 +0300

    Parallax background ve camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed = 2f;
+    public Transform target;
+    public float yOffset = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x,target.position.y+yOffset,-10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
+    }
+}
