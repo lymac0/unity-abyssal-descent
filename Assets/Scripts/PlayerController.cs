bfec8c2ba6225c4a1e0abe5096e970be2517d843
Kamil Sert <srtkamil42@gmail.com>
2025-02-18 23:20:32 +0300

    run, jump vs eklendi
    jump (start, end, landing)
    run (start, loop, end)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,56 +1,107 @@
 using System;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem.XInput;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float jumpSpeed;
+    private float groundSpeed;
     private Rigidbody2D body;
     private Animator anim;
+    public BoxCollider2D groundCheck;
+    public LayerMask groundMask;
+    private int jumpCount;
+
+    private float xInput;
+    private float yInput;
+
+    public int maxJumps = 2;
     public bool grounded;
-    public float drag;
+    [Range(0f, 1f)]
+    public float groundDecay;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        jumpCount = maxJumps;
     }
 
     private void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-
-        if (Mathf.Abs(xInput) > 0)
-            body.linearVelocity = new Vector2(xInput * speed, body.linearVelocity.y);
-        if (Mathf.Abs(yInput) > 0)
-            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * speed);
-
-
-        if (xInput > 0)
-            transform.localScale = new Vector3(math.abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-        if (xInput < 0)
-            transform.localScale = new Vector3(-math.abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        }
-
-        anim.SetBool("isWalking", xInput != 0);
+        GetInput();
+        MoveWithInput();
     }
 
     private void FixedUpdate()
     {
         CheckGround();
+        ApplyFriction();
+    }
 
-        if (grounded)
+    private void GetInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+    private void MoveWithInput()
+    {
+
+        if (Mathf.Abs(xInput) > 0)
         {
-            body.linearVelocity *= drag;
+            body.linearVelocity = new Vector2(xInput * groundSpeed, body.linearVelocity.y);
+            float direction = Mathf.Sign(xInput);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * direction, transform.localScale.y, transform.localScale.z);
+
+
+            if (Input.GetButton("Sprint"))
+            {
+                groundSpeed = runSpeed;
+                anim.SetBool("isRunning", true);
+            }
+            else
+            {
+                groundSpeed = walkSpeed;
+                anim.SetBool("isRunning", false);
+            }
+
+
         }
+
+        if (Mathf.Abs(yInput) > 0)
+            body.linearVelocity = new Vector2(body.linearVelocity.x, yInput * groundSpeed);
+
+
+        if (grounded && body.linearVelocity.y == 0)
+        {
+            jumpCount = maxJumps;
+        }
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpSpeed);
+            jumpCount--;
+        }
+
+
+        //anim.SetBool("isWalking", xInput != 0);
+        anim.SetFloat("xVelocity", Mathf.Abs(body.linearVelocity.x));
+        anim.SetFloat("yVelocity", body.linearVelocity.y);
+
     }
 
     private void CheckGround()
     {
+        grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+        anim.SetBool("isGrounded", grounded);
+
+    }
+
+    private void ApplyFriction()
+    {
+        if (grounded && xInput == 0)
+            body.linearVelocity *= groundDecay;
     }
 }
