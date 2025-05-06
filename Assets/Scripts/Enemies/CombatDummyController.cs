dcee07f6222ef4c0bcdee3d5e2daf5dfe602c41d
Kamil Sert <srtkamil42@gmail.com>
2025-05-06 19:46:38 +0300

    Basic Enemy with state machine

---
+++ Assets/Scripts/Enemies/CombatDummyController.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+
+public class CombatDummyController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealth, knockbackSpeedX, knockbackSpeedY, knockbackDuration, knockbackDeathSpeedX, knockbackDeathSpeedY, deathTorque;
+    [SerializeField]
+    private bool applyKnockback;
+    [SerializeField]
+    private GameObject HitParticle;
+
+    private float currentHealth, knockbackStart;
+
+    private int playerFacingDirection;
+
+    private bool playerOnLeft, knockback;
+
+    private PlayerController pc;
+    private GameObject aliveGO, brokenTopGO, brokenBotGO;
+    private Rigidbody2D rbAlive, rbBrokenTop, rbBrokenBot;
+    private Animator aliveAnim;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        aliveGO = transform.Find("Alive").gameObject;
+        brokenTopGO = transform.Find("Broken Top").gameObject;
+        brokenBotGO = transform.Find("Broken Bottom").gameObject;
+
+        aliveAnim = aliveGO.GetComponent<Animator>();
+        rbAlive = aliveGO.GetComponent<Rigidbody2D>();
+        rbBrokenTop = brokenTopGO.GetComponent<Rigidbody2D>();
+        rbBrokenBot = brokenBotGO.GetComponent<Rigidbody2D>();
+
+        aliveGO.SetActive(true);
+        brokenTopGO.SetActive(false);
+        brokenBotGO.SetActive(false);
+    }
+
+    void Update()
+    {
+        CheckKnockback();
+    }
+
+    private void Damage(float[] attackDetails)
+    {
+        currentHealth -= attackDetails[0];
+
+        if (attackDetails[1] < aliveGO.transform.position.x)
+        {
+            playerFacingDirection = 1;
+        }
+        else
+        {
+            playerFacingDirection = -1;
+        }
+
+        Instantiate(HitParticle, aliveAnim.transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+
+        if (playerFacingDirection == 1)
+        {
+            playerOnLeft = true;
+        }
+        else
+        {
+            playerOnLeft = false;
+        }
+
+        aliveAnim.SetBool("playerOnLeft", playerOnLeft);
+        aliveAnim.SetTrigger("damage");
+
+        if (applyKnockback && currentHealth > 0.0f)
+        {
+            //Knockback
+            Knockback();
+        }
+
+        if (currentHealth <= 0.0f)
+        {
+            //Die
+            Die();
+        }
+    }
+    private void Knockback()
+    {
+        knockback = true;
+        knockbackStart = Time.time;
+        rbAlive.linearVelocity = new Vector2(knockbackSpeedX * playerFacingDirection, knockbackSpeedY);
+    }
+
+    private void CheckKnockback()
+    {
+        if (Time.time >= knockbackStart + knockbackDuration && knockback)
+        {
+            knockback = false;
+            rbAlive.linearVelocity = new Vector2(0.0f, rbAlive.linearVelocityY);
+        }
+    }
+
+    private void Die()
+    {
+        aliveGO.SetActive(false);
+        brokenTopGO.SetActive(true);
+        brokenBotGO.SetActive(true);
+
+        brokenTopGO.transform.position = aliveGO.transform.position;
+        brokenBotGO.transform.position = aliveGO.transform.position;
+
+        rbBrokenBot.linearVelocity = new Vector2(knockbackSpeedX * playerFacingDirection, knockbackSpeedY);
+        rbBrokenTop.linearVelocity = new Vector2(knockbackDeathSpeedX * playerFacingDirection, knockbackDeathSpeedY);
+        rbBrokenTop.AddTorque(deathTorque * -playerFacingDirection, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCombatController.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerCombatController : MonoBehaviour
+{
+    [SerializeField]
+    private bool combatEnabled;
+    [SerializeField]
+    private float inputTimer, attack1Range, attack1Damage;
+    [SerializeField]
+    private Transform attack1HitBoxPos;
+    [SerializeField]
+    private LayerMask WhatIsDamageable;
+    private bool gotInput, isAttacking, isFirstAttack;
+
+    private float lastInputTime = Mathf.NegativeInfinity;
+    private float[] attackDetails = new float[2];
+    private Animator anim;
+
+    private PlayerController PC;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("canAttack", combatEnabled);
+        PC = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        CheckCombatInput();
+        CheckAttacks();
+    }
+
+    private void CheckCombatInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (combatEnabled)
+            {
+                gotInput = true;
+                lastInputTime = Time.time;
+            }
+        }
+    }
+
+    private void CheckAttacks()
+    {
+        if (gotInput)
+        {
+            if (!isAttacking)
+            {
+                gotInput = false;
+                isAttacking = true;
+                isFirstAttack = !isFirstAttack;
+                anim.SetBool("attack1", true);
+                anim.SetBool("firstAttack", isFirstAttack);
+                anim.SetBool("isAttacking", isAttacking);
+            }
+        }
+
+        if (Time.time >= lastInputTime + inputTimer)
+        {
+            gotInput = false;
+        }
+    }
+    private void CheckAttackHitbox()
+    {
+        Collider2D[] detectedObjects = Physics2D.OverlapCircleAll(attack1HitBoxPos.position, attack1Range, WhatIsDamageable);
+
+        attackDetails[0] = attack1Damage;
+        attackDetails[1] = transform.position.x;
+
+        foreach (Collider2D collider in detectedObjects)
+        {
+            collider.transform.parent.SendMessage("Damage", attackDetails);
+        }
+    }
+
+    private void FinishAttack1()
+    {
+        isAttacking = false;
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetBool("attack1", false);
+    }
+
+    private void Damage(float[] attackDetails)
+    {
+        if (!PC.GetDashStatus())
+        {
+            int direction;
+
+            // damage here using attackDetails[0] 
+
+            if (attackDetails[1] < transform.position.x)
+            {
+                direction = 1;
+            }
+            else
+            {
+                direction = -1;
+            }
+
+            PC.Knockback(direction);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attack1HitBoxPos.position, attack1Range);
+    }
+}
