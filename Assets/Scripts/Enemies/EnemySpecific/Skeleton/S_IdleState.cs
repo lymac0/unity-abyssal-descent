7a0ea7e772e66b0486c9446c0f9ac99027a68fa9
Kamil Sert <srtkamil42@gmail.com>
2025-06-02 03:27:52 +0300

    New enemy type Skeleton added
    Idle, Move, Dead, Charge, LookForPlayer, PlayerDetected states defined

---
+++ Assets/Scripts/Enemies/EnemySpecific/Skeleton/S_IdleState.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class S_IdleState : IdleState
+{
+    private Skeleton enemy;
+    public S_IdleState(Entity entity, FiniteStateMachine stateMachine, string animBoolName, D_IdleState stateData, Skeleton enemy) : base(entity, stateMachine, animBoolName, stateData)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (isPlayerInMinAgroRange)
+        {
+            stateMachine.ChangeState(enemy.playerDetectedState);
+        }
+        else if (isIdleTimeOver)
+        {
+            stateMachine.ChangeState(enemy.moveState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemySpecific/Skeleton/S_LookForPlayerState.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class S_LookForPlayerState : LookForPlayerState
+{
+    private Skeleton enemy;
+    public S_LookForPlayerState(Entity entity, FiniteStateMachine stateMachine, string animBoolName, D_LookForPlayer stateData, Skeleton enemy) : base(entity, stateMachine, animBoolName, stateData)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (isPlayerInMinAgroRange)
+        {
+            stateMachine.ChangeState(enemy.playerDetectedState);
+        }
+        else if (isAllTurnsTimeDone)
+        {
+            stateMachine.ChangeState(enemy.moveState);
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
