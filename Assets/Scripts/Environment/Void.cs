8da773f7108333e8b92bf8b83a6f232334340fc3
Kamil Sert <srtkamil42@gmail.com>
2025-06-03 21:12:42 +0300

    void fix

--- Assets/Scripts/Environment/Void.cs
+++ Assets/Scripts/Environment/Void.cs
@@ -3,18 +3,25 @@ using UnityEngine.UIElements;
 
 public class Void : MonoBehaviour
 {
-    public LayerMask whatIsPlayer;
+    public LayerMask whatIsEnemy;
     public AttackDetails attackDetails;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Oyuncu layer'indeyse
-        if (((1 << collision.gameObject.layer) & whatIsPlayer) != 0)
+        if (((1 << collision.gameObject.layer) & whatIsEnemy) != 0)
         {
             attackDetails.damageAmount = 9999; // Anýnda öldürecek kadar yüksek
             attackDetails.position = transform.position;
 
-            collision.transform.SendMessage("Damage", attackDetails);
+            if (collision.CompareTag("Player"))
+            {
+                collision.transform.SendMessage("Damage", attackDetails);
+            }
+            else if(collision.CompareTag("Enemy"))
+            {
+                collision.transform.parent.SendMessage("Damage", attackDetails);
+            }
         }
     }
 }
